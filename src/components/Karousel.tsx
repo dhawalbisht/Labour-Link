7da727dfc89ea217d363568bc0e0f7750022c63b
Asmeet Singh <54591619+asmeet3x@users.noreply.github.com>
2023-11-20 20:36:26 +0530

    Add files via upload

--- src/components/Karousel.tsx
+++ src/components/Karousel.tsx
@@ -11,18 +11,26 @@ const images = [
     "./assets/4.png",
     "./assets/5.png"
   ];
-function Karousel() {
-  return (
-    <div style={{paddingTop:100, height:300}} className='box'>
-        <Carousel useKeyboardArrows={true}>
-            {images.map((URL, index) => (
-                 <div className="slide">
-         <img alt="sample_file" src={URL} key={index} />
-         </div>
-    ))}
-  </Carousel> 
-    </div>
-  )
-}
-
-export default Karousel;
+  function Karousel() {
+    const customStyles = {
+      controlDots: {
+        width: '10px',
+        height: '10px',
+        margin: '0 4px', // Adjust spacing between dots if needed
+        backgroundColor: 'black', // Set the dot color to dark gray
+      },
+    };
+    return (
+      <div style={{ paddingTop: 100, height: 300 }} className='box'>
+        <Carousel useKeyboardArrows={true} autoPlay={true} interval={4000} showThumbs={false} showStatus={false} infiniteLoop={true} transitionTime={1000}>
+          {images.map((URL, index) => (
+            <div className="slide" key={index}>
+              <img alt={`sample_file_${index}`} src={URL}  style={{ marginBottom: '40px' }} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    )
+  }
+  
+  export default Karousel;
